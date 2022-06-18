8659edd0deb8180bb406ea0eb716842c702a3ba4
EC2 Default User <ec2-user@ip-172-31-92-181.ec2.internal>
2022-06-18 12:06:52 +0000

    [fix]public/addresses（表示・登録・編集・更新・削除を、ログインしたユーザーしか行えない・表示されないように修正

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,11 +1,13 @@
 class Public::AddressesController < ApplicationController
   def index
     @addresses = Address.all
+    @addresses = current_customer.addresses
     @address = Address.new
   end
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     if @address.save
       redirect_to public_addresses_path
     else
@@ -15,10 +17,16 @@ class Public::AddressesController < ApplicationController
 
   def edit
     @address = Address.find(params[:id])
+    if @address.customer_id == current_customer.id
+      render :edit
+    else
+      redirect_to public_addresses_path
+    end
   end
 
   def update
     @address = Address.find(params[:id])
+    @address.customer_id = current_customer.id
     if @address.update(address_params)
       redirect_to public_addresses_path
     else
@@ -28,8 +36,12 @@ class Public::AddressesController < ApplicationController
 
   def destroy
     @address = Address.find(params[:id])
-    @address.destroy
-    redirect_to public_addresses_path
+    if @address.customer_id == current_customer.id
+      @address.destroy
+      redirect_to public_addresses_path
+    else
+      render :index
+    end
   end
 
   private
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,8 @@
 class Address < ApplicationRecord
 
+
   belongs_to :customer
+
 
 
 end
